81b06cfe4b96de0b24d84d92d9cd8a6237b69696
alekskahawai <vol17004@byui.edu>
2023-06-15 12:54:43 +0100

    w01

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,3 +1,5 @@
+/* Prompt the user for their first name. Then, prompt them for their last name. Display the text back all on one line saying, "Your name is last-name, first-name, last-name" as shown: */
+
 using System;
 
 class Program
@@ -5,5 +7,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        /* Prompt the user for their first name. What is your first name? */
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+
+        /* prompt them for their last name. What is your last name? */
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        /* Display the text back all on one line saying, 
+        "Your name is last-name, first-name, last-name" as shown:  
+        Your name is Young, Brigham Young.
+        */
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
