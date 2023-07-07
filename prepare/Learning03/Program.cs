9f2374fb38d7da6725be23a2eb68e01cb8e633ad
alekskahawai <vol17004@byui.edu>
2023-07-07 18:29:00 +0100

    w05

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // verify that you can create fractions using all three of these constructors. 
+        // create an instance for 1/1 (using the first constructor), 
+        Fraction f1 = new Fraction();
+        // 6/1 (using the second constructor)
+        Fraction f2 = new Fraction(6);
+        // 6/7 (using the third constructor).
+        Fraction f3 = new Fraction(6, 7);
+
+        // verify that you can call all of these methods and get the correct values, using setters to change the values and then getters to retrieve these new values and then display them to the console.
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        /* Provide a menu that allows the user choose these options:
+
+        1. Write a new entry - Show the user a random prompt (from a list that you create), and save their response, the prompt, and the date as an Entry.
+
+        Your list of prompts must contain at least five different prompts. Make sure to add your own prompts to the list, but the following are examples to help get you started:
+            Who was the most interesting person I interacted with today?
+            What was the best part of my day?
+            How did I see the hand of the Lord in my life today?
+            What was the strongest emotion I felt today?
+            If I had one thing I could do over today, what would it be?
+            
+        2. Display the journal - Iterate through all entries in the journal and display them to the screen.
+
+        3. Save the journal to a file - Prompt the user for a filename and then save the current journal (the complete list of entries) to that file location.
+        
+        4. Load the journal from a file - Prompt the user for a filename and then load the journal (a complete list of entries) from that file. This should replace any entries currently stored the journal. 
+        
+        5. Exit
+        */
     }
 }
