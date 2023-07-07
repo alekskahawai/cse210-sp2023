0e4e0223f4c514bc42f3ca5b7c8118cf789441d5
alekskahawai <vol17004@byui.edu>
2023-07-07 21:03:21 +0100

    w03

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -3,53 +3,65 @@ using System;
 // Create a class to hold fraction.
 class Fraction
 {
-    /* The class should have two attributes for the top and bottom numbers.
-        Make sure the attributes are private. */
-        private int _topNumber;
-        private int _bottomNumber;
+    /* The class should have two attributes for the top and bottom numbers. 
+    Make sure the attributes are private. */
+    private int _topNumber;
+    private int _bottomNumber;
 
-        // Create the following constructors:
-        // Constructor that has no parameters that initializes the number to 1/1.
-        public Fraction()
-        {
-            _topNumber = 1;
-            _bottomNumber = 1;
+    // Create the following constructors:
+    // Constructor that has no parameters that initializes the number to 1/1.
+    public Fraction()
+    {
+        _topNumber = 1;
+        _bottomNumber = 1;
+    }
 
-        }
+    // Constructor that has one parameter for the top and that initializes the denominator to 1. So that if you pass in the number 5, the fraction would be initialized to 5/1.
+    public Fraction(int wholeNumber)
+    {
+        _topNumber = wholeNumber;
+        _bottomNumber = 1;
+    }
 
-        // Constructor that has one parameter for the top and that initializes the denominator to 1. So that if you pass in the number 5, the fraction would be initialized to 5/1.
-        public Fraction(int wholeNumber)
-        {
-            _topNumber = wholeNumber;
-            _bottomNumber = 1;
-        }
+    // Constructor that has two parameters, one for the top and one for the bottom.
+    public Fraction(int topNumber, int bottomNumber)
+    {
+        _topNumber = topNumber;
+        _bottomNumber = bottomNumber;            
+    }
 
-        // Constructor that has two parameters, one for the top and one for the bottom.
-        public Fraction(int topNumber, int bottomNumber)
-        {
-            _topNumber = topNumber;
-            _bottomNumber = bottomNumber;            
-        }
+    // Create getters and setters for both the top and the bottom values.
+    public int GetTopNumber()
+    {
+        return _topNumber;
+    }
 
-        // Create getters and setters for both the top and the bottom values.
-        public int GetTopNumber()
-        {
-            return _topNumber;
-        }
+    public void SetTopNumber(int topNumber)
+    {
+        _topNumber = topNumber;
+    }
 
-        public void SetTopNumber(int topNumber)
-        {
-            _topNumber = topNumber;
-        }
+    public int GetBottomNumber()
+    {
+        return _bottomNumber;
+    }
 
-        public int GetBottomNumber()
-        {
-            return _bottomNumber;
-        }
+    public void SetBottomNumber(int bottomNumber)
+    {
+        _bottomNumber = bottomNumber;
+    }
 
-        public void SetBottomNumber(int bottomNumber)
-        {
-            _bottomNumber = bottomNumber;
-        }
+    // Create a method called GetFractionString that returns the fraction in the form 3/4.
+    public string GetFractionString()
+    {
+        string fractionString = $"{_topNumber}/{_bottomNumber}";
+        return fractionString;
+    }
 
+    // Create a method called GetDecimalValue that returns a double that is the result of dividing the top number by the bottom number, such as 0.75.
+    public double GetDecimalValue()
+    {
+        double decimalValue = (double)_topNumber / (double)_bottomNumber;
+        return decimalValue;
+    }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,14 +4,85 @@ class Program
 {
     static void Main(string[] args)
     {
-        // verify that you can create fractions using all three of these constructors. 
-        // create an instance for 1/1 (using the first constructor), 
-        Fraction f1 = new Fraction();
-        // 6/1 (using the second constructor)
-        Fraction f2 = new Fraction(6);
-        // 6/7 (using the third constructor).
-        Fraction f3 = new Fraction(6, 7);
+        // // verify that you can create fractions using all three of these constructors. 
+        // // create an instance for 1/1 (using the first constructor), 
+        // Fraction f1 = new Fraction();
 
-        // verify that you can call all of these methods and get the correct values, using setters to change the values and then getters to retrieve these new values and then display them to the console.
+        // // 6/1 (using the second constructor)
+        // Fraction f2 = new Fraction(6);
+
+        // // 6/7 (using the third constructor).
+        // Fraction f3 = new Fraction(6, 7);
+
+        // // verify that you can call all of these methods and get the correct values, using setters to change the values and then getters to retrieve these new values and then display them to the console.
+        // // F1
+        // int f1Top = f1.GetTopNumber();
+        // int f1Bott = f1.GetBottomNumber();
+
+        // Console.WriteLine($"F1 top number: {f1Top}, bottom number: {f1Bott}");
+
+        // f1.SetTopNumber(2);
+        // f1.SetBottomNumber(2);
+
+        // int f1SetTop = f1.GetTopNumber();
+        // int f1SetBott = f1.GetBottomNumber();
+
+        // Console.WriteLine($"F1 New top number: {f1SetTop}, bottom number: {f1SetBott}");
+        // Console.WriteLine(); // Empty line
+
+        // // F2
+        // int f2Top = f2.GetTopNumber();
+        // int f2Bott = f2.GetBottomNumber();
+
+        // Console.WriteLine($"F2 top number: {f2Top}, bottom number: {f2Bott}");
+
+        // f2.SetTopNumber(3);
+        // f2.SetBottomNumber(3);
+
+        // int f2SetTop = f2.GetTopNumber();
+        // int f2SetBott = f2.GetBottomNumber();
+
+        // Console.WriteLine($"F2 New top number: {f2SetTop}, bottom number: {f2SetBott}");
+        // Console.WriteLine(); // Empty line
+
+        // // F3
+        // // int f3Top = f3.GetTopNumber();
+        // // int f3Bott = f3.GetBottomNumber();
+
+        // Console.WriteLine($"F3 top number: {f3.GetTopNumber()}, bottom number: {f3.GetBottomNumber}");
+
+        // f3.SetTopNumber(4);
+        // f3.SetBottomNumber(4);
+
+        // // int f3SetTop = f3.GetTopNumber();
+        // // int f3SetBott = f3.GetBottomNumber();
+
+        // Console.WriteLine($"F3 New top number: {f3.GetTopNumber()}, bottom number: {f3.GetBottomNumber}");
+
+        // Verify that you can call each constructor and that you can retrieve and display the different representations for a few different fractions. 
+        //For example, you could try:
+        // 1
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine(); // Empty line
+
+        // 5
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+        Console.WriteLine(); // Empty line
+
+        // 3/4
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+        Console.WriteLine(); // Empty line
+        
+        // 1/3
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+        Console.WriteLine(); // Empty line
     }
 }
