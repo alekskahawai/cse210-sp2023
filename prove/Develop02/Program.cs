082ddeeefcaf5a75ba79e02ddf6a18fb32cc5938
alekskahawai <vol17004@byui.edu>
2023-07-13 15:11:31 +0100

    w02

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,22 +1,37 @@
-/* This program must contain the following features:
+/* Submission comment.
+Creativity: Save other information in the journal entry.
 
-    1. Write a new entry - Show the user a random prompt (from a list that you create), and save their response, the prompt, and the date as an Entry.
+Sometimes people forget to read the Scriptures daily, so while writing a journal there is an additional question for each entry,  "Did you read the Scriptures today?"
 
-    Your list of prompts must contain at least five different prompts. Make sure to add your own prompts to the list, but the following are examples to help get you started:
-        Who was the most interesting person I interacted with today?
-        What was the best part of my day?
-        How did I see the hand of the Lord in my life today?
-        What was the strongest emotion I felt today?
-        If I had one thing I could do over today, what would it be?
-        
-    2. Display the journal - Iterate through all entries in the journal and display them to the screen.
+// Question about Scriptures
+Console.WriteLine("Did you read the Scriptures today?");
+Console.Write(">>> ");
+string scriptures = Console.ReadLine();
 
-    3. Save the journal to a file - Prompt the user for a filename and then save the current journal (the complete list of entries) to that file location.
+// creat an Entry instance
+Entry anEntry = new Entry(entryDate, promptText, entryText, scriptures);
+*/
+
+/* W02 Prove: Developer—Journal.
+This program must contain the following features:
+
+1. Write a new entry - Show the user a random prompt (from a list that you create), and save their response, the prompt, and the date as an Entry.
+
+Your list of prompts must contain at least five different prompts. Make sure to add your own prompts to the list, but the following are examples to help get you started:
+    Who was the most interesting person I interacted with today?
+    What was the best part of my day?
+    How did I see the hand of the Lord in my life today?
+    What was the strongest emotion I felt today?
+    If I had one thing I could do over today, what would it be?
     
-    4. Load the journal from a file - Prompt the user for a filename and then load the journal (a complete list of entries) from that file. This should replace any entries currently stored the journal. 
-    
-    5. Exit
-    */
+2. Display the journal - Iterate through all entries in the journal and display them to the screen.
+
+3. Save the journal to a file - Prompt the user for a filename and then save the current journal (the complete list of entries) to that file location.
+
+4. Load the journal from a file - Prompt the user for a filename and then load the journal (a complete list of entries) from that file. This should replace any entries currently stored the journal. 
+
+5. Exit
+*/
 using System;
 using System.IO;
 
@@ -73,7 +88,7 @@ class Program
 
                 // Creativity
                 // Question about Scriptures
-                Console.WriteLine("Did you read Scriptures today?");
+                Console.WriteLine("Did you read the Scriptures today?");
                 Console.Write(">>> ");
                 string scriptures = Console.ReadLine();
 
