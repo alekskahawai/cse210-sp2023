4ef4a0a4627d775ecf9bed203398ce76b6e85cf5
alekskahawai <vol17004@byui.edu>
2023-07-19 14:14:52 +0100

    w06

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -1,10 +1,22 @@
 public class Activity
 {
     // Attributes:
-    private string _date; 
+    private string _date; // 03 Nov 2022
     private int _length; // length of the activity in minutes
+    private string _activityType;
+
+    // constructor
+    // public Activity
 
     // Behaviors:
+    // *  GetSummary( )  :  string
+    // Sample Summary: 
+    // 03 Nov 2022 Running (30 min)- Distance 3.0 miles, Speed 6.0 mph, Pace: 10.0 min per mile
+    public string GetSummary()
+    {
+        return $"{_date} {_activityType} ({_length} min)- Distance {GetDistance()} km, Speed {GetSpeed()} km/hr, Pace: {GetPace()} min per km";
+    }
+
     // *  GetLength( )  :  int           // min
     public int GetLength()
     {
@@ -28,13 +40,4 @@ public class Activity
     {
         return 0;
     }
-
-    // *  GetSummary( )  :  string
-    // Sample Summary: 
-    // 03 Nov 2022 Running (30 min)- Distance 3.0 miles, Speed 6.0 mph, Pace: 10.0 min per mile
-    public string GetSummary()
-    {
-        return $"";
-    }
-
 }
